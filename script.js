f6d4132fd691ce6f313b9a75c1d22f7f1556b539
Fabi0o <marfab32@gmail.com>
2022-01-29 10:57:02 +0100

    add functions to player object that let player display marker on game board

--- script.js
+++ script.js
@@ -9,12 +9,30 @@ const gameboard = (() => {
   };
   return {
     displayBoard,
+    board,
   };
 })();
 //creates players
 const player = (number, marker) => {
-  return { number, marker };
+  const buttons = document.querySelectorAll(".gridItem");
+
+  const displayMove = function (event) {
+    gameboard.board[this.dataset.indexNumber] = marker;
+    gameboard.displayBoard();
+    buttons.forEach((button) => {
+      button.removeEventListener("click", displayMove);
+    });
+  };
+  const playerMove = () => {
+    buttons.forEach((button) => {
+      if (button.textContent == "") {
+        button.addEventListener("click", displayMove);
+      }
+    });
+  };
+  return { number, marker, playerMove };
 };
 
 const playerOne = player("playerOne", "X");
 const playerTwo = player("playerTwo", "O");
+playerOne.playerMove();
